a49a0bca5d71ccc48f3a299f9947cc43df326274
wngpfus <145317213+wngpfus@users.noreply.github.com>
2024-07-21 00:24:03 +0900

    Title: 좀 더 어려운 수학 점수, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240721/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,6 +1,6 @@
 am, ae = map(int, input().split())
 bm, be = map(int, input().split())
-if am >= bm or (am == bm and ae >= be ):
+if am > bm or (am == bm and ae >= be ):
     print('A')
 else:
     print('B')
