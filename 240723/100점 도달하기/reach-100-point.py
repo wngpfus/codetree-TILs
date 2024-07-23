81ff27f9a2deb01098b025b16e2ce236a4e8c96b
wngpfus <145317213+wngpfus@users.noreply.github.com>
2024-07-23 22:27:35 +0900

    Title: 100점 도달하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/100점 도달하기/reach-100-point.py
+++ 240723/100점 도달하기/reach-100-point.py
@@ -2,15 +2,12 @@ n = int(input())
 for i in range(n, 101):
     if n >= 90:
         print('A', end=' ')
-        n += 1
     elif n >= 80:
         print('B', end=' ')
-        n += 1
     elif n >= 70:
         print('C', end=' ')
-        n += 1
     elif n >= 60:
         print('D', end=' ')
     else:
         print('F', end=' ')
-        n += 1
+    n += 1
