1a61156b43e826a357db47a646b65ad2e4a9f939
wngpfus <145317213+wngpfus@users.noreply.github.com>
2024-08-05 22:13:27 +0900

    Title: 5의 배수의 합, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/5의 배수의 합/sum-of-specific-condition-multiple.py
+++ 240805/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,27 +1,28 @@
-# a, b = map(int, input().split())
-
-# sum = 0
-
-# if a <= b:
-#     for i in range(a, b + 1):
-#         if i % 5 == 0:
-#             sum += i
-# else:
-#     for i in range(b, a + 1):
-#         sum += i 
-
-# print(sum)
-
 a, b = map(int, input().split())
 
 sum = 0
 
-# 작은 값이 항상 a가 되도록 합니다.
-if a > b:
-    a, b = b, a
-
-for i in range(a, b + 1):
-    if i % 5 == 0:
-        sum += i
+if a <= b:
+    for i in range(a, b + 1):
+        if i % 5 == 0:
+            sum += i
+else:
+    for i in range(b, a + 1):
+        if i % 5 == 0:
+            sum += i 
 
 print(sum)
+
+# a, b = map(int, input().split())
+
+# sum = 0
+
+# # 작은 값이 항상 a가 되도록 합니다.
+# if a > b:
+#     a, b = b, a
+
+# for i in range(a, b + 1):
+#     if i % 5 == 0:
+#         sum += i
+
+# print(sum)
