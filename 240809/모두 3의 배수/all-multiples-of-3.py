833a3224b3d628dfd487bda9494c5863a3b8be67
wngpfus <145317213+wngpfus@users.noreply.github.com>
2024-08-09 15:15:34 +0900

    Title: 모두 3의 배수, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/모두 3의 배수/all-multiples-of-3.py
+++ 240809/모두 3의 배수/all-multiples-of-3.py
@@ -5,9 +5,11 @@ for i in range(5):
     n = int(input())
     if n % 3 == 0:
         cnt += 1
-    elif cnt == 5:
-        satisefite = True
-if satisefite == True:
-    print(0)
-else:
+        if cnt == 5:
+            satisefite = True
+            break  # 조건이 만족되면 더 이상 반복할 필요가 없으므로 반복문을 종료합니다.
+
+if satisefite:
     print(1)
+else:
+    print(0)
